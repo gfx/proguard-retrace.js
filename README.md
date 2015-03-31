6a464d83504ec73470e4bed71f1afd6512c033cc
Fuji, Goro (gfx) <goro-fuji@cookpad.com>
2015-03-31 19:27:09 +0900

    reformat

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 npm install -g typescript
 git submodules update --init # or just `git clone --recursive`
 make
-node bin/retrace
+node bin/retrace mapping.txt stacktrace.txt
 ```
 
--- src/proguard-retrace.ts
+++ src/proguard-retrace.ts
@@ -5,63 +5,62 @@ var fs = require("fs");
 
 var mappingFile = process.argv[2] || "mapping.txt";
 var stacktraceFile = process.argv[3] || "stacktrace.txt";
-console.log([mappingFile, stacktraceFile]);
 
-function readLines(file : string) : string[] {
-  return fs.readFileSync(file).toString().split(/\n/);
+function readLines(file: string): string[] {
+    return fs.readFileSync(file).toString().split(/\n/);
 }
 
 var mapping = {}; // (fqName, Data)
 var lineMapping = {};
 
-function decode(mapping : any, line : string) : string {
-  return line.replace(/\bat (\S+)\(SourceFile:(\d+)\)/, (matched, symbol, lineNumber, offset) => {
-    var originalSymbol = mapping[symbol] || symbol.replace(/(\S+)\.([\w\$\<\>]+)$/, (matched, klass, member) => {
-      //console.warn([matched, klass, member]);
-      return (mapping[klass] || klass) + "." + member;
-    });
+function decode(mapping: any, line: string): string {
+    return line.replace(/\bat (\S+)\(SourceFile:(\d+)\)/, (matched, symbol, lineNumber, offset) => {
+        var originalSymbol = mapping[symbol] || symbol.replace(/(\S+)\.([\w\$\<\>]+)$/, (matched, klass, member) => {
+                //console.warn([matched, klass, member]);
+                return (mapping[klass] || klass) + "." + member;
+            });
 
-    if (originalSymbol) {
-      var originalFile   = "SourceFile"; // TODO
-      var originalLineNumber = (lineMapping[symbol] ? lineMapping[symbol][lineNumber] : null) || lineNumber;
-      return "at " + originalSymbol + "(" + originalFile + ":" + originalLineNumber + ")";
-    }
-    else {
-      console.warn("XXX: " + symbol);
-      return matched;
-    }
-  });
+        if (originalSymbol) {
+            var originalFile = "SourceFile"; // TODO
+            var originalLineNumber = (lineMapping[symbol] ? lineMapping[symbol][lineNumber] : null) || lineNumber;
+            return "at " + originalSymbol + "(" + originalFile + ":" + originalLineNumber + ")";
+        }
+        else {
+            console.warn("XXX: " + symbol);
+            return matched;
+        }
+    });
 }
 
-var klass : string;
-var originalKlass : string;
+var klass: string;
+var originalKlass: string;
 readLines(mappingFile).forEach((line) => {
-  if (/^\S/.test(line)) { // class
-    var matched = /(\S+) -> (\S+):/.exec(line);
-    originalKlass = matched[1];
-    klass = matched[2];
+    if (/^\S/.test(line)) { // class
+        var matched = /(\S+) -> (\S+):/.exec(line);
+        originalKlass = matched[1];
+        klass = matched[2];
 
-    lineMapping[klass] = {};
-  }
-  else if(/\S/.test(line)) { // field
-    var matched = /(\S+)\s+(\S+) -> (\S+)/.exec(line);
+        lineMapping[klass] = {};
+    }
+    else if (/\S/.test(line)) { // field
+        var matched = /(\S+)\s+(\S+) -> (\S+)/.exec(line);
 
-    var lineAndType = matched[1];
-    var originalName = /[\w\$]+/.exec(matched[2])[0];
-    var name = matched[3];
+        var lineAndType = matched[1];
+        var originalName = /[\w\$]+/.exec(matched[2])[0];
+        var name = matched[3];
 
-    mapping[klass + "." + name] = originalKlass + "." + originalName;
+        mapping[klass + "." + name] = originalKlass + "." + originalName;
 
-    lineAndType.replace(/(\d+):(\d+)/, (_, to, from) => {
-      lineMapping[klass][to] = from;
-      return "dummy";
-    });
-  }
+        lineAndType.replace(/(\d+):(\d+)/, (_, to, from) => {
+            lineMapping[klass][to] = from;
+            return "dummy";
+        });
+    }
 });
 
 //console.log(mapping)
 
 readLines(stacktraceFile).forEach((line) => {
-  console.log( decode(mapping, line) );
+    console.log(decode(mapping, line));
 });
 
